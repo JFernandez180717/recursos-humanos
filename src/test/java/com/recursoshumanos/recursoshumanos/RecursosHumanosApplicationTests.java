7fac74856fd4e764cd9beab9851e2e1db8656fd8
JFernandez180717 <cta.cv2014@gmail.com>
2024-05-19 07:52:29 -0500

    primer commit

---
+++ src/test/java/com/recursoshumanos/recursoshumanos/RecursosHumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recursoshumanos.recursoshumanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursosHumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
